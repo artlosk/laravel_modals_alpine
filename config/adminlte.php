3f347ad9e2c5e272d0ef1e0b115978bb35fb98f1
loskutnikoff <artlosk@gmail.com>
2026-01-10 15:23:22 +0600

    update logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -108,7 +108,7 @@ return [
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Shopping List Logo',
+    'logo_img_alt' => 'Laravel Modals Logo',
 
     /*
     |--------------------------------------------------------------------------
